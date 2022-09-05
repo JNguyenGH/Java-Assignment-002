65ea2a15bfb4861e97c1a7e46f19a8189d6c802b
Jesse <nguyen.jesse0@gmail.com>
2022-09-04 17:39:12 -0700

    CIS-12 Assignment 2

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-
+        System.out.println("Please type the number of sides.");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double b = 360.0 / n;
+        double a = b / 2;
+        double s = 2 * Math.sin(Math.toRadians(a));
+        double p = n * s;
+        double pi = p / 2;
+        System.out.printf("Our PI estimate is: %f", pi);
     }
 }
